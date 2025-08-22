ca2200dfbb16b5749753daa0aa3313de4d573b2e
UNOsan118 <shin118h16@gmail.com>
2025-08-22 08:11:31 +0000

    Prepare the code for the international finals (copied from Japan).

---
+++ src/WRO2025_FE/src/international_final/launch/final_run.launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    """
+    This launch file starts ONLY the fuzzy logic driving node.
+    The controller and LIDAR are expected to be running already.
+    """
+    obstacle_navigator_node = Node(
+        package='international_final',
+        executable='run_obstacle_navigator',  # Use the executable name from setup.py
+        name='obstacle_navigator_node',   # Define a unique name for this node
+        output='screen',
+        emulate_tty=True, # Often useful for seeing colored logs
+    )
+
+    return LaunchDescription([
+        obstacle_navigator_node
+    ])
